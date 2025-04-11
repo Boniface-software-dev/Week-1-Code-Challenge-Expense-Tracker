608e1cde0512f37f4ef0a869bb71685eaf52e5c8
Boniface Kimani Muguro <mmbonifacekimani@gmail.com>
2025-04-12 00:40:30 +0300

    Personal Touch & Motivation paragraph

--- expense-tracker/src/App.jsx
+++ expense-tracker/src/App.jsx
@@ -26,6 +26,10 @@ function App() {
   return (
     <div className="container">
       <h1>Expense Tracker</h1>
+      <p> Budgeting isn’t about restriction, it’s about awareness. By tracking your expenses here, you’re
+  giving yourself the gift of clarity and control. Start small, stay consistent, and celebrate the
+  wins (yes, even the small ones).
+  You’ve got this!</p>
       <h3>Total Expenditure: Ksh {totalAmount.toFixed(2)}</h3>
       <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       <ExpenseForm onAddExpense={handleAddExpense} />
