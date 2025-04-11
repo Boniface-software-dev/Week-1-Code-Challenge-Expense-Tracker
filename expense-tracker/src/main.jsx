164ccf60d770fb6a987a348b0ce8ada7cb921e4d
Boniface Kimani Muguro <mmbonifacekimani@gmail.com>
2025-04-12 00:53:13 +0300

    footer

--- expense-tracker/src/main.jsx
+++ expense-tracker/src/main.jsx
@@ -5,7 +5,19 @@ import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <><React.StrictMode>
     <App />
   </React.StrictMode>
+  
+  <footer style={{
+    marginTop: '2rem',
+    padding: '1rem 0',
+    textAlign: 'center',
+    fontSize: '0.9rem',
+    color: '#777',
+    borderTop: '1px solid #ddd'
+  }}>
+      <p>© {new Date().getFullYear()} Expense Tracker.  Every dollar has a story. Log it, track it, understand it.</p>
+      <p>Track wisely. Spend mindfully.</p>
+    </footer></>
 )
